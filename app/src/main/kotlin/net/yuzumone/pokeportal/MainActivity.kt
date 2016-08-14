340955919182b21033dab5541015cc289d793f92
yuzumone <info@y-zu.net>
2016-08-14 20:28:41 +0900

    Add displaying the current location

--- app/src/main/kotlin/net/yuzumone/pokeportal/MainActivity.kt
+++ app/src/main/kotlin/net/yuzumone/pokeportal/MainActivity.kt
@@ -17,12 +17,48 @@
 
 package net.yuzumone.pokeportal
 
+import android.location.Location
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import net.yuzumone.pokeportal.listener.OnLocation
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnLocation, OnMapReadyCallback {
+
+    protected val TAG = "MainActivity"
+    lateinit private var mLocation: Location
+
+    override fun getLocation(location: Location) {
+        Log.d(TAG, location.toString())
+        mLocation = location
+        val fragment = MapFragment()
+        fragment.getMapAsync(this)
+        fragmentManager.beginTransaction().add(android.R.id.content, fragment).commit()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val position = LatLng(mLocation.latitude, mLocation.longitude)
+        googleMap?.addMarker(MarkerOptions().position(position))
+        val cameraPosition = CameraPosition
+                .builder()
+                .target(position)
+                .zoom(15f)
+                .build()
+        googleMap?.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragment = LocationFragment()
+        fragment.show(supportFragmentManager, "location")
     }
 }
